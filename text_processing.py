e53b2f6b91726a82ce7438819dd27f5c18dc3e7e
Byeongmin Choi <28807922+b8choi@users.noreply.github.com>
2021-01-19 17:08:26 +0900

    Implement functions

--- text_processing.py
+++ text_processing.py
@@ -9,54 +9,23 @@ NLP에서 흔히하는 전처리는 소문자 변환, 앞뒤 필요없는 띄어
 
 
 def normalize(input_string):
-    """
-     인풋으로 받는 스트링에서 정규화된 스트링을 반환함
-     아래의 요건들을 충족시켜야함
-    * 모든 단어들은 소문자로 되어야함
-    * 띄어쓰기는 한칸으로 되어야함
-    * 앞뒤 필요없는 띄어쓰기는 제거해야함
+    input_string = input_string.lower().strip()
+    words = input_string.split()
 
-         Parameters:
-             input_string (string): 영어로 된 대문자, 소문자, 띄어쓰기, 문장부호, 숫자로 이루어진 string
-             ex - "This is an example.", "   EXTRA   SPACE   "
+    normalized_string = ''
+    for s in words[:-1]:
+        normalized_string += s + ' '
+    normalized_string += words[-1]
 
-         Returns:
-             normalized_string (string): 위 요건을 충족시킨 정규회된 string
-             ex - 'this is an example.'
-
-         Examples:
-             >>> import text_processing as tp
-             >>> input_string1 = "This is an example."
-             >>> tp.normalize(input_string1)
-             'this is an example.'
-             >>> input_string2 = "   EXTRA   SPACE   "
-             >>> tp.normalize(input_string2)
-             'extra space'
-    """
-    normalized_string = None
     return normalized_string
 
 
 def no_vowels(input_string):
-    """
-    인풋으로 받는 스트링에서 모든 모음 (a, e, i, o, u)를 제거시킨 스트링을 반환함
+    no_vowel_string = ''
+    for c in input_string:
+        if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u':
+            continue
+        else:
+            no_vowel_string += c
 
-        Parameters:
-            input_string (string): 영어로 된 대문자, 소문자, 띄어쓰기, 문장부호로 이루어진 string
-            ex - "This is an example."
-
-        Returns:
-            no_vowel_string (string): 모든 모음 (a, e, i, o, u)를 제거시킨 스트링
-            ex - "Ths s n xmpl."
-
-        Examples:
-            >>> import text_processing as tp
-            >>> input_string1 = "This is an example."
-            >>> tp.normalize(input_string1)
-            "Ths s n xmpl."
-            >>> input_string2 = "We love Python!"
-            >>> tp.normalize(input_string2)
-            ''W lv Pythn!'
-    """
-    no_vowel_string = None
     return no_vowel_string
